a1bc1a65e59a8bd42cd2001170f7065a00625488
Alexandr <toledospb@gmail.com>
2020-01-21 17:23:18 +0300

    Firebase&gitignore settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Video Chat v.1(alpha)`,
+    description: `Video Chat Gatsby & Twilio`,
+    author: `@AlexCraftt`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
